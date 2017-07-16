2604d964b58fde25f9b6c9805624e3b1d37c6142
Xi Huang <xi.huang@morningstar.com>
2017-07-15 22:50:36 -0500

    adjusted scripts

--- src/index.js
+++ src/index.js
@@ -6,15 +6,14 @@ var host = 'http://174.129.85.123';
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = appId;
+    alexa.appId = appId;
     alexa.dynamoDBTableName = 'highLowGuessUsers';
-    alexa.registerHandlers(newSessionHandlers, guessModeHandlers, startGameHandlers, travelKitHandlers);
+    alexa.registerHandlers(newSessionHandlers, guessModeHandlers, travelKitHandlers);
     alexa.execute();
 };
 
 var states = {
     GUESSMODE: '_GUESSMODE', // User is trying to guess the number.
-    STARTMODE: '_STARTMODE',  // Prompt the user to start or restart the game.
     BRINGSTUFFMODE: '_BRINGSTUFFMODE'
 };
 
@@ -24,24 +23,10 @@ var newSessionHandlers = {
             this.attributes['endedSessionCount'] = 0;
             this.attributes['gamesPlayed'] = 0;
         }
-        this.handler.state = states.STARTMODE;
-        this.emit(':ask', 'Thanks for using Trip Planner. Please say Yes to get started.');
+        this.handler.state = states.GUESSMODE;
+        this.emit(':ask', 'Thanks for using Trip Planner. Please tell me where you want to go for how many days? For example, you can tell me I want to go to San Francisco for two days.');
     }
 };
-
-var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
-    'NewSession': function () {
-        this.emit('NewSession'); // Uses the handler in newSessionHandlers
-    },
-    'TempYesIntent': function() {
-        this.handler.state = states.GUESSMODE;
-        this.emit(':ask', 'Great! ' + 'Please tell me where you want to go for how many days? For example, you can tell me I want to go to San Francisco for two days.');
-    },
-    "AMAZON.StopIntent": function() {
-      console.log("STOPINTENT");
-      this.emit(':tell', "Goodbye!");  
-    }
-});
 
 var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
     'NewSession': function () {
@@ -61,7 +46,13 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
                 console.log('You want to go to ' + result + ' for ' + travelDays + ' days.');
                 _this.attributes['city_state'] = result;
                 _this.attributes['travel_days'] = travelDays;
-                _this.emit(':ask', 'You want to go to ' + result + ' for ' + travelDays + ' days? I will go ahead and plan your trip if you say Yes.');
+                var maximumDayOutputSpeech = travelDays > 5 ? "We only support five days trip planning. We have adjusted your travel days for five days. " : "";
+                if (travelDays > 5) {
+                    travelDays = 5;
+                }
+                var outputSpeech = maximumDayOutputSpeech + 'You want to go to ' + result + ' for ' + travelDays + ' days? I will go ahead and plan your trip if you say Yes.';
+
+                _this.emit(':ask', outputSpeech);
             });
     },
     'TempYesIntent': function() {
@@ -100,7 +91,7 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
                 _this.attributes['trips'][0].forEach(function(item){
                     listOfPointOfInterests += item.name + ", ";
                     cardContent += 'Place: ' + item.name + ' (' + item.duration +' mins)\n';
-                    // cardContent += 'Address: ' + item.address +'\n';
+                    cardContent += 'Address: ' + item.address +'\n';
                 });
                 imageObj.smallImageUrl = fullTrips[0].img_url;
                 imageObj.largeImageUrl = fullTrips[0].img_url;
@@ -110,12 +101,22 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
             });
     },
     'AMAZON.StopIntent': function() {
-      console.log("STOPINTENT");
-      this.emit(':tell', "Goodbye!");  
+        console.log("STOPINTENT");
+        this.emit(':tell', "Goodbye!");
+    },
+    'AMAZON.CancelIntent': function() {
+        console.log("CANCEL INTENT");
+        this.emit(':tell', "Ok! I canceled your request");  
     },
     'GetOtherDayDetail': function() {
         var theOtherDay = parseInt(this.event.request.intent.slots.theOtherDay.value);
-        console.log(" User requested for day " + theOtherDay + "trip details");
+        var exceptionSpeech = "";
+        if (theOtherDay > parseInt(this.attributes['travel_days']) || typeof(theOtherDay) !== "number") {
+            console.log(" User requested for day: " + theOtherDay + " trip details.");
+            console.log(" System adjusted days for program to process");
+            theOtherDay = 2;
+            exceptionSpeech = "You said you want to see Day " + theOtherDay + " trip details, which is invalid. We will show you Day 2 trip details by default. ";
+        }
         var cardTitle = this.attributes['city_state'] + " Trip: Day " + theOtherDay;
         var listOfPointOfInterests = '';
         var sentenceStart = 'Here is your Day ' + theOtherDay +' trip details.';
@@ -125,14 +126,14 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
         this.attributes['trips'][theOtherDay - 1].forEach(function(item){
             listOfPointOfInterests += item.name + ", ";
             cardContent += 'Place: ' + item.name + '\n';
-            // cardContent += 'Address: ' + item.address +'\n';
+            cardContent += 'Address: ' + item.address +'\n';
         });
         var imageObj = {
             smallImageUrl: '',
             largeImageUrl: ''
         };
         listOfPointOfInterests = listOfPointOfInterests.substr(0, listOfPointOfInterests.length - 2);
-        this.emit(':askWithCard', sentenceStart + ' You can go to: ' + listOfPointOfInterests + sentenceEnd, repromptSpeech, cardTitle, cardContent, imageObj);
+        this.emit(':askWithCard', exceptionSpeech + sentenceStart + ' You can go to: ' + listOfPointOfInterests + sentenceEnd, repromptSpeech, cardTitle, cardContent, imageObj);
     },
     'GrantListCardAccess': function() {
         var permissions = ["write::alexa:household:list"];
@@ -143,8 +144,8 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
     'ThingsToBringIntent': function() {
         var basicItems = ['Toothbrush', 'Toothpatse', 'Towel', 'Flip Flops'];
         var cardTitle = 'Travel Basic Package';
-        var cardContent = 'Please remembmer to bring: \n' + '1. '+ basicItems[0] + '\n' + '2. ' + basicItems[1]  + '\n' + '3. ' + basicItems[2]  + '\n' + '4. ' + basicItems[3] + '\n';
-        var sentenceEnd = 'You can say "Send Me Travel Kit". We will ship the travel kit to your Airbnb or hotels in the travel destination.';
+        var cardContent = 'Please remembmer to bring: \n' + basicItems[0] + ', \n' + basicItems[1]  + ', \n' + basicItems[2]  + ', \n' + basicItems[3] + '. \n';
+        var sentenceEnd = 'Do you want to know what other travelers bring?';
         var outputSpeech = cardContent + sentenceEnd;
         this.handler.state = states.BRINGSTUFFMODE;
         var imageObj = {
@@ -152,6 +153,10 @@ var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
             largeImageUrl: ''
         };
         this.emit(':askWithCard', outputSpeech, sentenceEnd, cardTitle, cardContent, imageObj);
+    },
+    'Unhandled': function () {
+        this.handler.state = states.GUESSMODE;
+        this.emit(':ask', 'Sorry, I don\'t get it! Please tell me which city you want to go.', 'Tell me the city you want to go and I plan for you');
     }
 });
 
@@ -179,16 +184,13 @@ var travelKitHandlers = Alexa.CreateStateHandler(states.BRINGSTUFFMODE, {
         var outputSpeech = returnResponse ? returnResponse.response : smartResponses[2].response;
         this.emit(':tellWithCard', outputSpeech, cardTitle, outputSpeech);
     },
-    'SendMePackages': function () {
-        var basicItems = ['Toothbrush', 'Toothpatse', 'Towel', 'Flip Flops'];
-        var cardTitle = 'Preparing to send Travel Kit to your Airbnb.';
-        var cardContent = 'Travel Kit items: \n' + '1. '+ basicItems[0] + '\n' + '2. ' + basicItems[1]  + '\n' + '3. ' + basicItems[2]  + '\n' + '4. ' + basicItems[3] + '\n';
-        var sentenceEnd = 'Do you want to know what other travelers suggest to bring?';
-        var outputSpeech = 'Great! I am preparing to ship the travel kit to you now.' + sentenceEnd;
-        var imageObj = {
-            smallImageUrl: '',
-            largeImageUrl: ''
-        };
-        this.emit(':askWithCard', outputSpeech, sentenceEnd, cardTitle, cardContent, imageObj);
+    'AMAZON.StopIntent': function() {
+        console.log("STOPINTENT");
+        this.emit(':tell', "Goodbye!");
+    },
+    'AMAZON.CancelIntent': function() {
+        console.log("CANCEL INTENT");
+        this.handler.state = states.GUESSMODE;
+        this.emit(':tell', "Ok! I canceled your request");  
     }
 });
